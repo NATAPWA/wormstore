85806e2ba2d34e307e6d0442425211dac22af942
NATAPWA <anatas777@rambler.ru>
2026-07-15 22:41:52 +0400

    Update app.js

--- app.js
+++ app.js
@@ -6,15 +6,15 @@
         installBtn.classList.remove('hidden');
         installBtn.addEventListener('click', function() {
             showModal(
-                'Установите Червяк',
-                'Сейчас откроется Safari. Нажмите кнопку "Поделиться" (прямоугольник со стрелкой) внизу экрана, затем выберите "На экран Домой" и нажмите "Добавить".',
+                'Установите Червяк Jobs',
+                'Сейчас откроется Safari. Нажмите значок «Поделиться» (квадрат со стрелкой) внизу экрана. Затем нажмите на галочку «Показать больше» в правом углу и выберите «На экран Домой», нажмите «Добавить».',
                 'https://natapwa.github.io/wormstore/'
             );
         });
     }
 })();
 
-// ========== Установка приложений (MAX, VK) ==========
+// ========== Установка приложений ==========
 (function() {
     function bindButtons() {
         var buttons = document.querySelectorAll('.install-app-btn');
@@ -25,42 +25,43 @@
                 var app = card.getAttribute('data-app');
                 var url = '';
                 var name = '';
+
                 if (app === 'max') {
                     name = 'MAX';
                     url = 'https://web.max.ru';
                 } else if (app === 'vk') {
                     name = 'VK Мессенджер';
                     url = 'https://vk.com/messenger';
-                }
                 } else if (app === 'sberbank') {
-    name = 'СберБанк Онлайн';
-    url = 'https://online.sberbank.ru';
-} else if (app === 'gosuslugi') {
-    name = 'Госуслуги';
-    url = 'https://www.gosuslugi.ru';
-} else if (app === 'yandexmaps') {
-    name = 'Яндекс.Карты';
-    url = 'https://yandex.ru/maps';
-} else if (app === 'ozon') {
-    name = 'Ozon';
-    url = 'https://ozon.ru';
-} else if (app === 'wildberries') {
-    name = 'Wildberries';
-    url = 'https://wildberries.ru';
-} else if (app === 'avito') {
-    name = 'Авито';
-    url = 'https://www.avito.ru';
-} else if (app === 'youmoney') {
-    name = 'ЮMoney';
-    url = 'https://money.yandex.ru';
-} else if (app === 'dzen') {
-    name = 'Дзен';
-    url = 'https://dzen.ru';
-}
+                    name = 'СберБанк Онлайн';
+                    url = 'https://online.sberbank.ru';
+                } else if (app === 'gosuslugi') {
+                    name = 'Госуслуги';
+                    url = 'https://www.gosuslugi.ru';
+                } else if (app === 'yandexmaps') {
+                    name = 'Яндекс.Карты';
+                    url = 'https://yandex.ru/maps';
+                } else if (app === 'ozon') {
+                    name = 'Ozon';
+                    url = 'https://ozon.ru';
+                } else if (app === 'wildberries') {
+                    name = 'Wildberries';
+                    url = 'https://wildberries.ru';
+                } else if (app === 'avito') {
+                    name = 'Авито';
+                    url = 'https://www.avito.ru';
+                } else if (app === 'youmoney') {
+                    name = 'ЮMoney';
+                    url = 'https://money.yandex.ru';
+                } else if (app === 'dzen') {
+                    name = 'Дзен';
+                    url = 'https://dzen.ru';
+                }
+
                 if (url) {
                     showModal(
                         'Установите ' + name,
-                        'Сейчас откроется Safari. Нажмите кнопку "Поделиться" (прямоугольник со стрелкой) внизу экрана, затем выберите "На экран Домой" и нажмите "Добавить".',
+                        'Сейчас откроется Safari. Нажмите значок «Поделиться» (квадрат со стрелкой) внизу экрана. Затем нажмите на галочку «Показать больше» в правом углу и выберите «На экран Домой», нажмите «Добавить».',
                         url
                     );
                 }
